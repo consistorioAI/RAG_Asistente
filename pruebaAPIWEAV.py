eb4f9291c4f3d4a293af243a171ff3314ce80678
Ramón González <ramon@arnanz.es>
2025-06-24 13:35:49 +0200

    Fallo al levantar weaviate container 3

--- pruebaAPIWEAV.py
+++ pruebaAPIWEAV.py
@@ -3,7 +3,7 @@ from weaviate.auth import AuthApiKey
 
 client = weaviate.Client(
     url="http://localhost:8080",
-    auth_client_secret=AuthApiKey(api_key="clave_super_secreta")  # usa el mismo valor de .env
+    #auth_client_secret=AuthApiKey(api_key="clave_super_secreta")  # usa el mismo valor de .env
 )
 
 print(client.is_ready())  # ✅ Esto debe devolver True
